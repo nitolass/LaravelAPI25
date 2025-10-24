40a7f7aa29591ac99f45b4cb771c38e5c378a70c
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-24 21:17:27 +0200

    Paso 2

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::all());
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
     }
 }
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -14,6 +14,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->words(asText: true),
+            'description' => $this->faker->sentence(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
