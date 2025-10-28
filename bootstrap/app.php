45d3442ba6119673b162dd87001cba4d3150f91f
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-28 16:19:40 +0100

    Paso 3.5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->prependToGroup('api', App\Http\Middleware\AlwaysAcceptJSONMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function(NotFoundHttpException $e, Request $request){
+            if($request->wantsJson()){
+                return response()->json(['message' => 'Object Not Found'], 404);
+            }
+        });
     })->create();
