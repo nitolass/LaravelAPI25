7adac4b9e2980763dd697abc90ad79af17a90202
PORTATILHP\nito9 <tamboleojuan2005@gmail.com>
2025-10-29 19:09:35 +0100

    Paso 8

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,6 @@ Route::get('/user', function (Request $request) {
 
 Route::get('lists/categories',[CategoryController::class, 'list']);
 
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('categories/{category}', [CategoryController::class, 'show']);
-Route::post('categories', [CategoryController::class, 'store']);
-Route::put('categories/{category}', [CategoryController::class, 'update']);
-Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+Route::apiResource('categories', CategoryController::class);
 
 Route::get('products', [ProductController::class, 'index']);
